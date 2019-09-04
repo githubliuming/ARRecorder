13b97d8692351e1531a5bac1f6cf12d346529b8e
liuming <3165440298@qq.com>
2019-09-04 09:43:26 +0800

    添加 QYARRender 类

---
+++ ARRecorder/QYRecordCore/Rendering/QYARRender.h
@@ -0,0 +1,17 @@
+//
+//  QYARRender.h
+//  ARRecorder
+//
+//  Created by Yuri Boyka on 2019/9/4.
+//  Copyright © 2019 Yuri Boyka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QYARRender : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ARRecorder/QYRecordCore/Rendering/QYARRender.m
@@ -0,0 +1,13 @@
+//
+//  QYARRender.m
+//  ARRecorder
+//
+//  Created by Yuri Boyka on 2019/9/4.
+//  Copyright © 2019 Yuri Boyka. All rights reserved.
+//
+
+#import "QYARRender.h"
+
+@implementation QYARRender
+
+@end
